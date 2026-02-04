81208c45dc3c40d4cc2526f1a2df0ac98c268061
Yuvarajkurri <kurriyuvaraj13@gmail.com>
2026-02-05 03:00:42 +0530

    Error Login  Solved

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,17 @@ const dontenv = require("dotenv");
 dontenv.config();
 
 const app = express();
-app.use(cors({
-  origin: "https://jobforher.in"
-}));
 
+
+app.use(cors({
+  origin: [
+    "https://jobforher.in",
+    "http://127.0.0.1:5500",
+    "http://localhost:5500"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
